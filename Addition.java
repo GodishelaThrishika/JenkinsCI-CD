713da85397fb295707ed4df1bca8af2e47d8c231
Thrishika <godiselathrishika@gmail.com>
2025-07-18 15:55:34 +0530

    Added Addition.java program

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+public static int add(int a, int b){
+return a+b;
+}
+public static void main(string [] args){
+int sum = add(15,20);
+System.out.println("Sum :" + sum);
+}
+}
